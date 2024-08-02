6656c1d03ce2c28c59aa6fddb760d2624ac85924
John <deejay.gvr@gmail.com>
2024-08-02 13:06:34 +0200

    classe astratto + libro e rivista

---
+++ src/main/java/johnguevarra/Rivista.java
@@ -0,0 +1,27 @@
+package johnguevarra;
+
+public class Rivista extends ElementoCatalogo {
+    public enum Periodicita {
+        SETTIMANALE,
+        MENSILE,
+        SEMESTRALE
+    }
+
+    private Periodicita periodicita;
+
+    public Rivista(String isbn, String titolo, int annoPubblicazione, int numeroPagine, Periodicita periodicita) {
+        super(isbn, titolo, annoPubblicazione, numeroPagine);
+        this.periodicita = periodicita;
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    @Override
+    public String toString() {
+        return "Rivista{" +
+                "periodicita=" + periodicita +
+                "} " + super.toString();
+    }
+}

923f365673339d40d5d6e23178d058cd768f27ac
John <deejay.gvr@gmail.com>
2024-08-02 13:26:54 +0200

    creazione main

---
+++ src/main/java/johnguevarra/Main.java
@@ -0,0 +1,18 @@
+package johnguevarra;
+
+public class Main {
+    public static void main(String[] args) {
+        Catalogo catalogo = new Catalogo();
+
+        Libro libro1 = new Libro("978-3-16-148410-0", "Il Signore degli Anelli", 1954, 1216, "J.R.R. Tolkien", "Fantasy");
+        Rivista rivista1 = new Rivista("978-1-23-456789-7", "National Geographic", 2023, 120, Rivista.Periodicita.MENSILE);
+
+        catalogo.aggiungiElemento(libro1);
+        catalogo.aggiungiElemento(rivista1);
+
+        catalogo.stampaCatalogo();
+
+        ElementoCatalogo elementoCercato = catalogo.cercaPerIsbn("978-3-16-148410-0");
+        System.out.println("Elemento trovato: " + elementoCercato);
+    }
+}
